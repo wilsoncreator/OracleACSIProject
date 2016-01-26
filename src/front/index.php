7a063c7994c3afb330ab5c0a19c65f9b79bc0238
nathan-rossignol <nathan.rossignol@epsi.fr>
2016-01-26 11:41:36 +0100

    new commit

--- src/front/index.php
+++ src/front/index.php
@@ -1,3 +1,20 @@
 <?php
+
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../css/style.css">
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<form class="form-signin">
+    <input type="text" class="form-control" placeholder="Email" name="login" required autofocus><br>
+    <input type="password" class="form-control" placeholder="Password" name="password" required><br>
+    <button class="btn btn-lg btn-primary btn-block" id="button-login" type="submit">Sign in</button>
+</form>
+</body>
+</html>
